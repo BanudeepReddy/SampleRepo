bb3dbddc17c27b108f7199e8917645b934e7f34a
BanudeepReddy <banudeep_reddy@persistent.com>
2022-08-02 14:37:15 +0530

    Adding all files from VM

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+	}
+
+	public void display(){
+		System.out.println("This is display method");
+	}
+	
+	public void apicall(){
+		System.out.println("Calling API");
+	}
+
+}
